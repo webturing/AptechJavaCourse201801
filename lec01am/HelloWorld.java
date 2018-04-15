95d2aa58dd6c904c1ca61c52f7cd4c7ec04a89fa
unknown <zj@webturing.com>
2018-04-15 14:19:21 +0800

    lec01am finished

---
+++ lec01am/HelloWorld.java
@@ -0,0 +1,7 @@
+package lec01am;
+
+public class HelloWorld {// 主类名必须是Main
+	public static void main(String[] args) { // Java的主方法
+		System.out.println("This is my first c program!"); // 标准输出
+	}
+}
---
+++ lec01am/P1002Root2.java
@@ -0,0 +1,23 @@
+package lec01am;
+
+import java.util.Scanner;
+
+//import java.lang.Math;//String System
+public class P1002Root2 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		double a = cin.nextDouble();
+		double b = cin.nextDouble();
+		double c = cin.nextDouble();
+		double delta = b * b - 4 * a * c;
+		double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+		double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+		if (x1 < x2) {
+			double t = x1;
+			x1 = x2;
+			x2 = t;
+		}
+		System.out.printf("%.2f %.2f\n", x1, x2);
+		cin.close();
+	}
+}
