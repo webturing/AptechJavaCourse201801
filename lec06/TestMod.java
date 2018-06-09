0fa1a27d0544ecbccaf9b288521d6cd2e870c6d3
zj <zj@webturing.com>
2018-06-09 16:47:29 +0800

    算术模 浮点比较

---
+++ lec06/TestMod.java
@@ -0,0 +1,8 @@
+package lec06;
+
+public class TestMod {
+    public static void main(String[] args) {
+        int n = -18;
+        System.out.println((n % 10 + 10) % 10);
+    }
+}
--- zj/util/MathTool.java
+++ zj/util/MathTool.java
@@ -1,6 +1,9 @@
 package zj.util;
 
 public class MathTool {
+
+    public static final double EPS = 1e-6;
+
     /**
      * 计算 a,b的最大公约数
      *
@@ -19,9 +22,28 @@ public class MathTool {
     }
 
     public static void main(String[] args) {
+        //testGCD();
+        System.out.println(safeMod(-8, 6));
+        System.out.println(safeMod(8, 6));
+        float f = 1.0f;
+        float y = 0.12f + 0.880001f;
+        System.out.println(fEqual(f, y));
+    }
+
+    private static boolean fEqual(float x, float y) {
+        return Math.abs(x - y) <= EPS;
+    }
+
+    public static int safeMod(int a, int b) {
+        return (a % b + b) % b; //程序语言取模缺陷
+    }
+
+    private static void testGCD() {
         System.out.println(2 == gcd(6, 8));
         System.out.println(2 == gcd(6, -8));
         System.out.println(33 == gcd(66, 99));
         System.out.println(6 == gcd(6, 0));
     }
+
+
 }
