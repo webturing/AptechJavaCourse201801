daef6989cdd2bcfe756b49f4a8fef956a8907a7b
zj <zj@webturing.com>
2018-06-09 15:35:48 +0800

    分数的单元测试类

--- lec06/ex2/TestFraction.java
+++ lec06/ex2/TestFraction.java
@@ -2,13 +2,9 @@ package lec06.ex2;
 
 public class TestFraction {
     public static void main(String[] args) {
-        //testRationalize();
-        // testAdd();
-        Fraction sum = Fraction.ZERO;
-        for (int i = 1; i <= 20; i++) {
-            sum = sum.add(new Fraction(1, i));
-        }
-        System.out.println(sum);
+        testRationalize();
+        testAdd();
+
 
     }
 
